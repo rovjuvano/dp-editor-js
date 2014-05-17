a3f3c6e7813bc9d57baf0f0a249f1693582cdea0
Robert Juliano <rovjuvano@gmail.com>
2014-05-17 15:36:00 -0400

    chore: initial setup

---
+++ Gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+	clean = require('gulp-clean'),
+	jadeToHtml = require('gulp-jade'),
+	lessToCss = require('gulp-less'),
+	pkg = require('./package.json');
+
+var paths = {
+	jade: ['./src/**/*.jade'],
+	less: ['./src/**/*.less'],
+	raw: ['./src/**/*.js',
+		'bower_components/angular/angular.min.js{,.map}',
+	],
+}
+
+const DIST_DIR = './dist';
+
+gulp.task('default', ['build', 'watch']);
+gulp.task('clean', function() {
+	gulp.src(DIST_DIR, {read: false})
+		.pipe(clean());
+})
+
+gulp.task('build', ['build:jade', 'build:less', 'build:raw']);
+gulp.task('build:jade', function() {
+	gulp.src(paths.jade)
+		.pipe(jadeToHtml())
+		.pipe(gulp.dest(DIST_DIR));
+});
+gulp.task('build:less', function() {
+	gulp.src(paths.less)
+		.pipe(lessToCss())
+		.pipe(gulp.dest(DIST_DIR));
+});
+gulp.task('build:raw', function() {
+	gulp.src(paths.raw)
+		.pipe(gulp.dest(DIST_DIR));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.jade, ['build:jade']);
+	gulp.watch(paths.less, ['build:less']);
+	gulp.watch(paths.raw, ['build:raw']);
+});
